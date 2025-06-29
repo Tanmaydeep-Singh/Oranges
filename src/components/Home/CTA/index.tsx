8d0a1edbdb9304462f2d1640d35d404da33ddca4
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2025-06-29 16:18:11 +0530

    dev: updated CTA Section

--- src/components/Home/CTA/index.tsx
+++ src/components/Home/CTA/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const FomoBanner = () => {
   return (
-    <section className="bg-gradient-to-r from-orange-400 to-red-400 text-white py-12 px-6 text-center rounded-2xl shadow-lg my-12">
+    <section className=" py-12 px-6 text-center  my-12">
       <h2 className="text-3xl md:text-4xl font-bold mb-4">
         Don’t Miss Out on Smarter Learning 🚀
       </h2>
@@ -11,9 +11,9 @@ const FomoBanner = () => {
         Join thousands of students already growing with <span className="font-semibold">Oranged</span>.  
         Seats for early access are filling fast — secure your spot today!
       </p>
-      <button className="bg-white text-orange-600 hover:bg-gray-100 px-6 py-3 text-lg font-medium rounded-full">
-        Get Early Access Now
-      </button>
+      <button className="px-6 py-3 rounded-2xl bg-orange-500 text-white font-semibold text-lg shadow-md hover:bg-orange-600 transition-all duration-300">
+          Join the Beta
+        </button>
     </section>
   );
 };
