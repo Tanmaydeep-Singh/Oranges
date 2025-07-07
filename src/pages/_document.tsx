b3c3ed8ca44a0c5507b90f909d8927f6aded1e66
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2025-07-07 21:45:06 +0530

    dev: added faivon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,7 +38,7 @@ export default function Document() {
         <meta name="twitter:image" content="/logos/orange-logo.png" />
 
         {/* ✅ Favicon + Manifest */}
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
